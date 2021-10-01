ac540ecc810f6ffe3eea1cef06fd42e642182177
jeanlucj <jeanluc.work@gmail.com>
2021-10-01 17:06:26 -0400

    Simplified LoadPackages so it would just throw an error, not try to install...

--- LoadPackages.R
+++ LoadPackages.R
@@ -3,12 +3,12 @@ if (!exists("init_num")) init_num <- 1
 print("*************************")
 print(paste("Loading packages for", init_num))
 print("*************************")
-ip <- installed.packages()
-packages_used <- c("AlphaSimR", "dplyr")
-for (package in packages_used){
-  if (!(package %in% ip[,"Package"])) install.packages(package, repos="https://cloud.r-project.org")
-}
-library(dplyr)
 
-system("R CMD INSTALL -l /Library/Frameworks/R.framework/Versions/4.0/Resources/library BreedSimCost.tar.gz")
+ip <- installed.packages()
+packages_used <- c("AlphaSimR", "BreedSimCost", "dplyr")
+for (package in packages_used){
+  if (!(package %in% ip[,"Package"])) stop(paste("Missing package", package))
+}
+
+library(dplyr)
 library(BreedSimCost)
